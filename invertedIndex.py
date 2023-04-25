3d8374d38f60bf21eb32caa3a62f7cb42ba70024
mahdipro <mahdihamedi79@gmail.com>
2023-04-25 23:02:51 +0330

    1- increase stopwords 2- clean code 3- document for code

--- invertedIndex.py
+++ invertedIndex.py
@@ -1,5 +1,6 @@
 import os
 import xml.sax
+import json
 from nltk.corpus import wordnet 
 from nltk.stem import WordNetLemmatizer
 
@@ -33,82 +34,164 @@ class ReviewHandler(xml.sax.ContentHandler):
     def characters(self, content):
         self.current_data = content.strip()
 
-if __name__ == "__main__":
-    handler = ReviewHandler()
+handler = ReviewHandler()
 
-    root_directory = "./wellFormatedXML"
-    # Loop through all directories and subdirectories
-    for dirpath, dirnames, filenames in os.walk(root_directory):
-        # Loop through all files in the current directory
-        for filename in filenames:
-            file_path = os.path.join(dirpath, filename)
 
-            # parse the XML file using the handler
-            xml.sax.parse(file_path, handler)
 
-    # print the list of docs
+
+
+def Fetch_Docs():
+    # make a dictionary with following format => {"DocID": ,"Body": ,"Name": } from XML file.
+
+    # Test
+    xml.sax.parse('o.txt', handler)
+    docs = handler.docs[3]["Body"]
+    # Test
+
+
+
+    # Main
+    # root_directory = "./wellFormatedXML"
+    # for dirpath, dirname,filenames in os.walk(root_directory):
+    #     for filename in filenames:
+    #         file_path = os.path.join(dirpath, filename)
+
+    #         # parse the XML file using the handler
+    #         xml.sax.parse(file_path, handler)
+
+    # docs = ''
+    # for i in range(len(handler.docs)):
+    #     docs = docs + handler.docs[i]["Body"]
+    #Main
+
+
+
     # print(handler.docs)
-    print(len(handler.docs))
+    # print(handler.docs[5])
+    # print(len(handler.docs))
     # print(type(handler.docs[5]))
     # print(handler.docs[20]["DocId"])
     # print(handler.docs[20000]["Body"])
-    # print(handler.docs[5])
+
+
+
+    docs= handler.docs
+    # docs is a dictionary with following format => {"DocID": ,"Body": ,"Name": }
+    return docs
 
 
 
 
-    #Tokenization
-    docs = handler.docs[20]["Body"]
-    # for i in range(len(handler.docs)):
-    #     docs = docs + handler.docs[i]["Body"]
 
 
-    # print(len(docs))
-    translation_table = str.maketrans('\+\-\@\$\#\%\^\&\*0987654321\!\?\)\(\,\;\\.\"\\\/', '                                              ')
-    docs = docs.translate(translation_table)
-    tokens = docs.lower().split(" ")
+def Dict2Str(dict):
+
+    # get a dictionary with following format => {"DocID": ,"Body": ,"Name": } and return its "Body" key
+
+    String =''
+    for i in range(len(dict)):
+        String = String + dict[i]["Body"]
+
+    return String
+
+
+
+
+
+def Cleaner(string):
+
+    # input = string
+    # output = token
+
+    translation_table = str.maketrans('\+\-\=\_\@\$\#\%\^\&\*0987654321\!\?\)\(\,\;\\.\"\\\/',
+                                       '                                                  ')
+    string = string.translate(translation_table)
+    tokens = string.lower().split(" ")
     # print(len(tokens),'start')
     tokens = list(set(tokens))
     # print(len(tokens),'after removing duplicate values and special chars')
-    
+
     with open('./stopwords.txt', 'r') as f:
         stopwords = [line.rstrip() for line in f.readlines()]
     tokens = [x for x in tokens if x not in stopwords]
     # print(len(tokens),'after removeing \'\' and stop words')
-    
+
+    return tokens
 
 
-    # Lemmatization
+
+
+
+
+def Lemmatizer(wordList):
 
     # nltk.download('wordnet')
     lemmatizer = WordNetLemmatizer()
-    lemmatized_words = [lemmatizer.lemmatize(word, wordnet.VERB) for word in tokens]
+    lemmatized_words = [lemmatizer.lemmatize(word, wordnet.VERB) for word in wordList]
     lemmatized_words = list(set(lemmatized_words))
     # print(lemmatized_words)
     # print(len(lemmatized_words),'after removing duplicate values due to words with the same stem like loving, loved => love ')
+    return lemmatized_words
+
+
+
+
+
+
+def Tokenizer(text):
     
+    return Lemmatizer(Cleaner(text))
 
 
 
-    ### start making Inverted Index
-    tokens= lemmatized_words
+
+def invertedIndex(tokens, docs):
+
+    # input: list and dictionary with following format:
+    #    tokens: ['Word1','Word2','Word3', ...]
+    #    docs  : {"DocID": ,"Body": ,"Name": }
+
+    # output: dictionary as inverted index with following format:
+    #          {
+    #           "Word1":{"df": x ,"docs":[DocId ,tf]},
+    #           "Word2":{"df": x ,"docs":[DocId ,tf]},
+    #           "Word3":{"df": x ,"docs":[DocId ,tf]}
+    #           }
+
+
+    # df === Document Frecuency
+    # tf === Term Frecuency
+
     index={}
-    docs = handler.docs
+    tokens = list(tokens)
 
     for m in range(len(tokens)):
-        # df === Document Frecuency
+
         PostingList={"df":0, "docs":[]}
         for n in range(len(docs)):
 
             if tokens[m] in docs[n]["Body"]:
 
-                # tf === Term Frecuency
                 tf = docs[n]["Body"].count(tokens[m])#مشخص کردن تعداد کلمه در داکیومتت 
+                # if tf >= 20:
+                    # print(tokens[m]," : ",tf)
                 PostingList["docs"].append([docs[n]["DocId"],tf])# اضافه کردن عای دی و تعداد به اتریبیوت داکس هر  کلمه
                 PostingList["df"] = PostingList["df"] + 1
                 index[tokens[m]] = PostingList
-                # print(PostingList)
-    
-    # print(index)
-    print(len(index))
 
+                # index[tokens[m]] = json.dumps(PostingList)
+                # final_index = json.dumps(index)
+
+    return index
+
+
+
+
+
+# To see the process step by step, Run the following lines one by one
+
+# print(Fetch_Docs())
+# print(Dict2Str(Fetch_Docs()))
+# print(Tokenizer(Dict2Str(Fetch_Docs())))
+print(invertedIndex(Tokenizer(Dict2Str(Fetch_Docs())),Fetch_Docs()))
+print("\n\nuncomment lines 54-64 and comment lines 47-48 to make index with origin data.\n")
